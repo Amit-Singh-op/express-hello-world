a32a8385a2d6420eaa7f1f67ebbe24b8e52fd165
Amit Kumar Singh <66426717+Amit-Singh-op@users.noreply.github.com>
2024-03-14 03:13:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send{message:"damn bro its working"});
+app.get("/", (req, res) => res.send({message:"damn bro its working"}));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
