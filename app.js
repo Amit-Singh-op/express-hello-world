abf93dfe7731e3d9d4bd8da143b1d76c525e502d
Amit Kumar Singh <66426717+Amit-Singh-op@users.noreply.github.com>
2024-03-14 03:18:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+var http = require('http');
+var socket = require('socket.io');
+const app = http.createServer(server);
+const io = socket(app);
+app.listen(80);
 
 app.get("/", (req, res) => res.send({message:"damn bro its working"}));
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
