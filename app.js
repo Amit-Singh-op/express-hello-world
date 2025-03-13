5a36c15958f6d870631b34d23ae4eb2a70b282f3
Amit Kumar Singh <66426717+Amit-Singh-op@users.noreply.github.com>
2025-03-13 22:10:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const io = require("socket.io");
+const express=require("express")
 
-const server = io.listen(3000);
+const app=express()
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
+app.get("/",(req,res)=>{
+  return res.send({message:"hello world"})
+})
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.listen(5000, () => console.log(`Server running on port 5000`));
